c8fbed42e4729be860a66db13abcf22094a62fab
MaheshSumb <maheshsumbpatil87@gmail.com>
2025-09-24 03:55:28 +0530

    Created Project Structure and Created Some Required Entity

---
+++ src/main/java/com/kronos/api/KronosApiApplication.java
@@ -0,0 +1,14 @@
+package com.kronos.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KronosApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KronosApiApplication.class, args);
+		System.out.println("Application Started............");
+	}
+
+}
---
+++ src/test/java/com/kronos/api/KronosApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kronos.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KronosApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
